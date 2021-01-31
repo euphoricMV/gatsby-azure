09ed2d6b07a7c0389868d8619151eeec6956748d
Mladen <mladen.stankovic@rtl.nl>
2021-01-31 01:09:54 +0100

    Modify about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
 
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
 export default ({ data }) => (
-  <React.Fragment>
+  <Layout>
+    <SEO title="About" />
     <h2>{data.site.siteMetadata.title}</h2>
     <div>{data.site.siteMetadata.description}</div>
     <Link to="/">Back to home</Link>
-  </React.Fragment>
+  </Layout>
 )
 
 export const query = graphql `
